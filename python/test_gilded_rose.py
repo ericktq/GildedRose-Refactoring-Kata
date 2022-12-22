84b0b2990265c6a9dbb13a6e3a1e85901a66936a
ericktq <64106507+ericktq@users.noreply.github.com>
2022-12-22 12:11:52 -0500

    Add files via upload

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -9,7 +9,20 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
+    
+    def test_sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(80, items[0].quality)
+    
+    def test_cake(self):
+        items = [Item("Conjured Mana Cake", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("Conjured Mana Cake", items[0].name)
+
 
         
 if __name__ == '__main__':
--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 
 from gilded_rose import *
 
+Action_1="Backstage passes to a TAFKAL80ETC concert"
 if __name__ == "__main__":
     print ("OMGHAI!")
     items = [
@@ -11,9 +12,9 @@ if __name__ == "__main__":
              Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+             Item(name=Action_1, sell_in=15, quality=20),
+             Item(name=Action_1, sell_in=10, quality=49),
+             Item(name=Action_1, sell_in=5, quality=49),
              Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
 
